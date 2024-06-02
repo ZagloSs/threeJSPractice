ff6d3eb35bfdfc31a49d2f2664be166ef291fc78
ZagloSs <carlosdiezmendez@gmail.com>
2024-06-02 13:38:57 +0200

    it works

--- threejsPractice/main.js
+++ threejsPractice/main.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { ThreeMFLoader } from "three/examples/jsm/Addons.js";
+
 
 
 //Scene
@@ -8,26 +8,23 @@ const scene = new THREE.Scene();
 //Geometry
 const mesh = new THREE.Mesh(
     new THREE.SphereGeometry(3, 64, 64),
-    new THREE.MeshStandardMaterial({color:"#00ff83"})
+    new THREE.MeshStandardMaterial({color:"#00ff83",})
 );
 scene.add(mesh);
 
+//Light
+const light = new THREE.PointLight(0xffffff, 1, 18, 0);
+light.position.set(0,10,10)
+scene.add(light)
 
 //Camera
-const camera = new THREE.PerspectiveCamera(
-    45,
-    800/600
-);
+const camera = new THREE.PerspectiveCamera(45, 800 / 600);
 camera.position.z = 20
 scene.add(camera)
 
-//Light
-const light = new THREE.PointLight(0xffffff, 1, 100)
-light.position.set(0, 10, 10)
-scene.add(light)
 
 // Renderer
-var canvas = document.querySelector(".webgl");
+const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas});
 renderer.setSize(800, 600);
 
